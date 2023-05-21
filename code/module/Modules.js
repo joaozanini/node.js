57ca7df7c31591db3bfcd92e492f28f160fc7569
João Vitor <joaozanini.code@gmail.com>
2023-05-21 20:29:35 -0300

    .

---
+++ code/module/Modules.js
@@ -0,0 +1,9 @@
+const names = require('./names')
+
+const sayHi = (name) => {
+    console.log(`Hello ${name}`)
+}
+
+sayHi(names.susan)
+sayHi(names.john)
+sayHi(names.peter)
---
+++ info.js
@@ -0,0 +1,6 @@
+// __dirname  - caminho do diretório atual
+// __filename - nome do arquivo
+// require    - funcão para usar modulos
+// module     - informação sobre o modulo atual
+// .export    - exporta um módulo
+// process    - informação sobre env onde o programa esta sendo executado
